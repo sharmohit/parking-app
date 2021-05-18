516ba69e78cfea87a3179e3e225f9e5a842a3968
Javtesh <javteshsingh.bhullar@georgebrown.ca>
2021-05-18 21:25:13 +0530

    Created parking detail UI - Added DetailScreenViewController

---
+++ ParkingApp/Views/DetailScreenViewController.swift
@@ -0,0 +1,43 @@
+
+//  Group: Project Groups 12
+//  Name: Javtesh Singh
+//  Student ID: 101348129
+//  Group Member: Mohit Sharma
+//  Member ID: 101342267
+
+
+//  DetailScreenViewController.swift
+//  ParkingApp
+//
+//  Created by Javtesh Singh on 18/05/21.
+//
+
+import UIKit
+
+class DetailScreenViewController: UIViewController {
+    
+    @IBOutlet weak var lblBulidingCode: UILabel!
+    
+    
+    @IBOutlet weak var lblNumberOfHours: UILabel!
+    
+    @IBOutlet weak var lblSuitNumber: UILabel!
+    
+    @IBOutlet weak var lblLocationOfCar: UILabel!
+    
+    @IBOutlet weak var lblDateAndTime: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func getDirection(_ sender: Any) {
+        
+        print(#function,"getDirection pressed")
+        
+    }
+    
+}
